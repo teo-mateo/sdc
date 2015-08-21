56650c146ee6d738709708a76150a934ee33738a
Teodor B <tbardici@gmail.com>
2015-08-21 19:43:07 +0300

    login/register works

---
+++ SDC.web/Models/SDCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SDC.web.Models
+{
+    public class SDCContext : DbContext
+    {
+        public SDCContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
