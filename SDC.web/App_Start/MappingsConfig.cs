46212038c8ca1fd7ea86a783d0960d41b7ba1841
teo-mateo <teomati@gmail.com>
2015-09-10 15:36:47 +0200

    c

---
+++ SDC.web/App_Start/MappingsConfig.cs
@@ -0,0 +1,29 @@
+﻿using SDC.web.Models.Books;
+using SDC.web.Models.Common;
+using SDC.web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SDC.web.App_Start
+{
+    public class MappingsConfig
+    {
+        public static void RegisterMappings()
+        {
+            int i = 1;
+            AutoMapper.Mapper.CreateMap<Author, AuthorViewModel>()
+                .ForMember(vm => vm.AddedBy,
+                opts => opts.MapFrom(src => (src.AddedBy == null ? "-" : src.AddedBy.UserName)))
+                .ForMember(vm => vm.LastModifiedBy,
+                opts => opts.MapFrom(src => (src.LastModifiedBy == null ? "-" : src.AddedBy.UserName)));
+
+            AutoMapper.Mapper.CreateMap<AuthorViewModel, Author>();
+
+            AutoMapper.Mapper.CreateMap<Book, BookViewModel>()
+                .ForMember(vm => vm.Language,
+                opts => opts.MapFrom(src => new Language { Code = src.Language, Name = src.Language }));
+        }
+    }
+}
